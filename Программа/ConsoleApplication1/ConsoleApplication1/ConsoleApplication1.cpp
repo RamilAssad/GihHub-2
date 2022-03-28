8c615853bdcdd4ce11bc47663d023b43b7d8b330
RamilAssad <ramil_tolubaev@mail.ru>
2022-03-28 17:22:11 +0300

    Delete 8 commit

--- Программа/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Программа/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,13 +1,1 @@
 ﻿
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "Hello World!\n";
-    cout << "1";
-    int a, b;
-    a = 12;
-    b = 13;
-    cout << "a = " << a << "b = " << b;
-}
-
